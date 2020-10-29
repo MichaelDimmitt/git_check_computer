23ae1ab7019ea661af3299ab0d869b48438caa36
MichaelDimmitt <michaelgdimmitt@gmail.com>
2020-10-29 15:40:40 -0400

    add dots every 2 seconds to show the program is running and making progress

--- git_check_computer.sh
+++ git_check_computer.sh
@@ -1,3 +1,14 @@
+run_with_dots() {
+  # https://stackoverflow.com/a/25904176/5283424
+  "$@" &
+  while kill -0 $!
+  do
+      printf '.' > /dev/tty;
+      sleep 2;
+  done
+  printf '\n' > /dev/tty;
+}
+
 create_persist_file() {
   echo "Please enter any folders seperated by spaces with child git directories";
   echo "If no folder is provided ~/ will be used by default";
@@ -6,7 +17,7 @@ create_persist_file() {
   echo -e " \033[5;32;47m pass! no longer awaiting input: \033[0m"
   echo "Working on creating a ~/.persist file, this may take 4 minutes future lookups will be much faster!";
   echo "searching the following directory: ~/"$answer;
-  find $HOME$answer -type d -name .git 2>/dev/null > ~/.persist.txt;
+  run_with_dots find $HOME$answer -type d -name .git 2>/dev/null > ~/.persist.txt;
   cat .persist.txt | grep -v "Library/" > ~/.persist.txt;
   cat .persist.txt | wc -l;
 };
