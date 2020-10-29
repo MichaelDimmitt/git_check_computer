1e59596fb8504aab57d9719d626de9ac3b529b6b
MichaelDimmitt <michaelgdimmitt@gmail.com>
2020-10-29 15:37:36 -0400

    prompt user and let them know what directory is being searched

--- git_check_computer.sh
+++ git_check_computer.sh
@@ -1,5 +1,10 @@
 create_persist_file() {
-  find $HOME -type d -name .git 2>/dev/null > ~/.persist.txt;
+  echo "Please enter any folders seperated by spaces with child git directories";
+  echo "If no folder is provided ~/ will be used by default";
+  read answer;
+  echo "Working on creating a ~/.persist file, this may take 4 minutes future lookups will be much faster!";
+  echo "searching the following directory: ~/"$answer;
+  find $HOME$answer -type d -name .git 2>/dev/null > ~/.persist.txt;
   cat .persist.txt | grep -v "Library/" > ~/.persist.txt;
   cat .persist.txt | wc -l;
 };
